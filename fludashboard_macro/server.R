6cce1ab06fccc08b93122058064797d1b300ff26
Fernando Ferreira <fernando@twistsystems.com>
2021-02-13 21:25:01 -0300

    :construction: First dashboard with real data and map.

--- fludashboard_macro/server.R
+++ fludashboard_macro/server.R
@@ -1,17 +1,99 @@
-library(shiny)
+shhh <- suppressPackageStartupMessages
+list.of.packages <- c('shiny',
+                      'tidyverse',
+                      'here',
+                      'shinycssloaders',
+                      'leaflet')
 
+new.packages <- list.of.packages[!(list.of.packages %in%
+                                       installed.packages()[, "Package"])]
+if (length(new.packages))
+    install.packages(new.packages, dependencies = TRUE)
+
+# Importar pacotes necessário
+for (package in list.of.packages) {
+    shhh(library(package, character.only = TRUE))
+}
+options(bitmapType = "cairo")
+
+source(here("fludashboard_macro/nowcasting_v2.R"))
+
+# Shape info
+macros_file <- "fludashboard_macro/geobrazilmacrosaude.RDS"
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    macsaud.id <- shiny::reactiveVal(0)
+    output$mapBrazil <- renderLeaflet({
+        macros_saude <- readRDS(here(macros_file))
+        labels <- paste(macros_saude$DS_UF_SIGLA, "-", macros_saude$DS_ABREV_macsaud)
+        leaflet() %>%
+            addTiles() %>%
+            addPolygons(
+                data = macros_saude$geom,
+                fillColor="#ffffcc",
+                weight = 2,
+                color="#707070",
+                fillOpacity = 0.7,
+                layerId=macros_saude$CO_MACSAUD,
+                highlight = highlightOptions(
+                    weight = 5,
+                    color="#666",
+                    fillOpacity = 0.9,
+                    bringToFront = TRUE),
+                label = labels
+            ) %>%
+            setView(-50, -11, 4)
+    })
+    output$trendPlot <- renderPlot({
+        if(length(macsaud.id()) == 0)
+            return (NA);
+        xbreaks <- c(1, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52)
+        xlimits <- c(1, 52)
+        macro_name <- as.character(macros_saude %>%
+                                   as.data.frame() %>%
+                                   filter(CO_MACSAUD == macsaud.id()) %>%
+                                   select(DS_ABREV_macsaud))
+        uf <- as.character(macros_saude %>%
+                           as.data.frame() %>%
+                           filter(CO_MACSAUD == macsaud.id()) %>%
+                           select(DS_UF_SIGLA))
+        pred.srag.summy <- readRDS(here("fludashboard_macro/data/pred_srag_summy_1102.rds"))
+        p.now.srag <- plot.nowcast(pred.srag.summy, Fim=today.week ) +
+          ylab("Incidência de SRAG (por 100mil hab.)") +
+          xlab("Semana de primeiros sintomas") +
+          scale_x_continuous(breaks = xbreaks, limits = xlimits) +
+        #theme_Publication(base_size = 16, base_family = 'Roboto') +
+          ggtitle(paste0(uf, ": ", macro_name)) +
+          theme(plot.margin=unit(c(1,0,5,5), units='pt'),
+                axis.text = element_text(size = rel(1)),
+                legend.margin = margin(0,0,0,0, unit='pt'),
+                legend.justification=c(0,1),
+                legend.position=c(0.015, 1.05),
+                legend.background = element_blank(),
+                legend.key = element_blank(),
+                legend.key.size = unit(14, 'pt'),
+                legend.text = element_text(family = 'Roboto', size = rel(1)))
+        return(p.now.srag)
+    }, height = "auto")
+    output$MACSAUD <- renderText({
+        if (length(macsaud.id()) == 0)
+            text <- "Selecione uma macrorregião"
+        else {
+            macro_name <- as.character(macros_saude %>%
+                                     as.data.frame() %>%
+                                     filter(CO_MACSAUD == macsaud.id()) %>%
+                                     select(DS_ABREV_macsaud))
+            uf <- as.character(macros_saude %>%
+                                   as.data.frame() %>%
+                                   filter(CO_MACSAUD == macsaud.id()) %>%
+                                   select(DS_UF_SIGLA))
+            text <- paste(uf, "-", macro_name)
+        }
+        text
     })
 
+    observe({
+        event <- input$mapBrazil_shape_click
+        macsaud.id(event$id)
+    })
 })
--- fludashboard_macro/ui.R
+++ fludashboard_macro/ui.R
@@ -1,40 +1,106 @@
-
 # Packages that will be loaded for this application
+shhh <- suppressPackageStartupMessages
 list.of.packages <- c('shiny',
                       'tidyverse',
                       'here',
                       'shinycssloaders',
-                      'bootstraplib')
+                      'leaflet',
+                      'sf')
+
 new.packages <- list.of.packages[!(list.of.packages %in%
                                        installed.packages()[, "Package"])]
 if (length(new.packages))
     install.packages(new.packages, dependencies = TRUE)
 
-# Importar pacotes necessário
 for (package in list.of.packages) {
-    library(package, character.only = TRUE)
+    shhh(library(package, character.only = TRUE))
 }
 options(bitmapType = "cairo")
 
-addHeader <- function(contentDiv){
-    header <- includeHTML('www/super-header.html')
-    contentDiv <- tagAppendChild(contentDiv, div(class='row', header))
+getNavBar <- function() {
+    navbar <- tags$div(
+        tags$a(
+            tags$strong("Monitoramento de casos de síndrome respiratória aguda grave (SRAG) notificados no SIVEP-Gripe"),
+            class="navbar-brand-custom", href=""
+        ),
+        class = "navbar-header", style = "margin: 5px"
+    )
 }
 
-addFooter <- function(contentDiv){
-    contentDiv <- tagAppendChild(contentDiv, div(class='row-fluid',
-                                                 id="footer-brasil"))
+addHeader <- function(contentDiv) {
+   headerContent <- includeHTML(here("fludashboard_macro/www/super-header.html"))
+   headerContent <- tags$div(
+       tagList(headerContent, getNavBar()),
+       class = "navbar navbar-dark bg-primary",
+       role = "navigation"
+   )
+   contentDiv <- tagAppendChild(contentDiv, headerContent)
 }
 
-getContent <- function(){
-    contentDiv <- div(class="container-fluid") %>%
+addContent <- function(contentDiv){
+    mapBrazil <- leafletOutput("mapBrazil", height = 700)
+    sidepanel <- tagList(
+        tags$h1(textOutput("MACSAUD")),
+        tags$div(plotOutput("trendPlot"))
+    )
+    panel <- fluidRow(column(6, mapBrazil), column(6, sidepanel))
+    tabs <- tabsetPanel(
+        type = "tabs",
+        tabPanel(
+            "Macrorregiões Saúde",
+            panel
+        ),
+        tabPanel("Capitais", tags$div())
+    )
+    contentDiv <- tagAppendChild(contentDiv, tabs)
+}
+
+addFooter <- function(contentDiv) {
+    contentDiv <- tagAppendChild(contentDiv, div(class = 'row-fluid',
+                                                 id = "footer-brasil"))
+}
+
+
+
+getContent <- function() {
+    contentDiv <- div(class = "container-fluid") %>%
         addHeader() %>%
+        addContent() %>%
         addFooter()
 }
+headerStyle <- HTML('
+    html, document, body {
+        margin: 0!important;
+        padding: 0!important;
+        background: #dfdfdf;
+        line-height: 0.8;
+    }
+    .tab-content {
+        background: white!important;
+    }
+    .navbar-brand-custom {
+        font-size: 2rem;
+        line-height: 1;
+        color: #fff;
+        font-weight: bolder;
+    }
+    img.align-left, .figure.align-left, object.align-left {
+        float: left ;
+        margin-right: 1em }
 
-shinyUI(
-    bootstrapPage(
-        title = "Infogripe",
-        getContent()
-    )
-)
+    img.align-right, .figure.align-right, object.align-right {
+        float: right ;
+        margin-left: 1em }')
+
+headerStyle <- tagList(tags$style(headerStyle),
+                      tags$link(rel="stylesheet",
+                                href="http://www.ensp.fiocruz.br/portal-ensp/_estilos/ensp_barra-fiocruz.css",
+                                type="text/css",
+                                media="all"))
+headerStyle <- tags$head(headerStyle)
+
+shinyUI(bootstrapPage(headerStyle,
+                      getContent(),
+                      tags$script(defer="defer",
+                                  src="//barra.brasil.gov.br/barra_2.0.js",
+                                  type="text/javascript")))
